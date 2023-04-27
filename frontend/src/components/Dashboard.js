6314e104a93f8aaadd06410307f7ab4e0af812b9
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-04-27 14:47:14 +0200

    Fetch ok from rawg

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,5 +1,10 @@
 import React from "react";
+import GameShop from "./GameShop";
 
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+export default function Dashboard({ games }) {
+  return (
+    <div>
+      <GameShop games={games} />
+    </div>
+  );
 }
--- frontend/src/components/GameShop.js
+++ frontend/src/components/GameShop.js
@@ -1,5 +1,29 @@
 import React from "react";
 
-export default function GameShop() {
-  return <div>GameShop</div>;
+export default function GameShop({ games }) {
+  return (
+    <div>
+      {/* TO BE CHANGED INTO <GAMECARD/> INSTEAD!*/}
+      <section>
+        <h2>Gameshop</h2>
+        <button>Visit shop</button>
+        {games?.map((game, index) => (
+          <article key={index}>
+            <img
+              src={game.background_image}
+              alt={game.name}
+              style={{ maxWidth: "300px" }}
+            />
+            <h3>{game.name}</h3>
+            <ul>
+              {game.genres.map((genre, index) => (
+                <li key={index}>{genre.name}</li>
+              ))}
+            </ul>
+            <button>buy</button>
+          </article>
+        ))}
+      </section>
+    </div>
+  );
 }
