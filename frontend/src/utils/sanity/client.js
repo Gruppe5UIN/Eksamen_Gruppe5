a6b735fc6e79c22fbb187a440a0c479c96a9ae35
Cecilie Seim <totilie8@gmail.com>
2023-04-28 19:21:22 +0200

    comments

--- frontend/src/utils/sanity/client.js
+++ frontend/src/utils/sanity/client.js
@@ -8,7 +8,7 @@ export const client = createClient({
     apiVersion: "2021-10-21"
 })
 
-//skrive klient v/behov med token
+//skrive klient med token
 export const writeClient = createClient({
     token: 'skZH6g0Rmw0Z0CLqf3dQPm8BC3GPHdbOusf06vI249A4LBURltNWHeUglEamWPxaAR9DsBwnZeUYQS7s6BGlDHiJjcU287wr8RQw1RyETF2nKhvLNvQvojl9x9EYfTxcb1yLzK8n1N3rYNQOJ5FFTs0dBvAUyurbmDKBqOQbVOykwM59aCMs',
     projectId: '3j0bfcpd',
--- frontend/src/utils/sanity/gameServices.js
+++ frontend/src/utils/sanity/gameServices.js
@@ -1,8 +1,3 @@
 import { client } from "./client"
 
-//fetch all my games
-//fetch one game
-//fetch alle genres
-//fetch one genre (?)
-//fetch all favourites
-//fetch all favourites of one genre
+//fetch games/genres/favourites/myGames osv
