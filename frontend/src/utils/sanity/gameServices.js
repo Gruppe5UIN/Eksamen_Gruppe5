23253d6c16e2722896aff25531f8cfb0c3178c2c
Cecilie Seim <totilie8@gmail.com>
2023-05-03 16:26:23 +0200

    Import sanity client globally

--- frontend/src/utils/sanity/gameServices.js
+++ frontend/src/utils/sanity/gameServices.js
@@ -1,3 +1,8 @@
 import { client } from "./client"
 
 //fetch games/genres/favourites/myGames osv
+export const fetchAllGenres = async () =>{
+
+    const data = await client.fetch(`*[_type == "genre"]{apiId,title}`)
+    return data;
+}
