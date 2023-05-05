bce70f9faaf04492821094fece2765d214bb14bd
Cecilie Seim <totilie8@gmail.com>
2023-05-05 22:11:56 +0200

    Added tags

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -4,26 +4,21 @@ import { GiHeartShield } from "react-icons/gi";
 import { useParams } from "react-router-dom";
 import GameTable from "./GameTable";
 
-//Komponent for presentasjon av et spill basert på apiId
-//Trenger hjelp til å få til en fetch fra to kilder: et game objekt og en liste med screenshots (bilder)
+//Komponent for presentasjon av et spill basert på slug - som er unik og fungerer som id hos rawg
 
-//Dessuten forbedre koden som er, legge til feilhåndtering og utnytte useEffect bedre(?)
+//Legge til feilhåndtering 
 
 //Skal ha funksjonalitet for:
-//tags - wordCloud
-//favourite - må onClick legge til i favourites
-//button - kjøpe eller innlogget (?)
-
-//Tar ut tag i eget komponent
+//favourite - må onClick legge til i favourites - legge til i state
+//button - kjøpe eller innlogget bruker...
 
 export default function GamePage() {
   const { slug } = useParams();
 
   const [game, setGame] = useState();
-  //const [images, setImages] = useState();
+  const [favourite, setFavourite] = useState([]);
 
   const url = `https://api.rawg.io/api/games/${slug}?key=6ccebb406ca942cd8ddc8584b1da9a4f`;
-  //const imageUrl = `https://api.rawg.io/api/games/${apiId}/screenshots?key=6ccebb406ca942cd8ddc8584b1da9a4f`
 
   const getGame = async () => {
     const response = await fetch(url);
@@ -31,8 +26,9 @@ export default function GamePage() {
     setGame(data);
   };
 
+
   useEffect(() => {
-    getGame();
+    getGame() 
     // eslint-disable-next-line
   }, []);
 
@@ -42,13 +38,8 @@ export default function GamePage() {
     console.log(`${game?.name} er min favoritt`);
   };
 
-  //Bilder kan ev også få et eget komponent ?,
-  //særlig hvis vi vil ha en slider/bytte mellom hovedbilder og miniatyrer
-  //bør ha en alt tekst som skiller de ulike bildene..har bare satt på et nummer nå
+  //lazy loading images
   const mainImage = game?.background_image;
-  const miniature1 = game?.background_image_additional;
-  const miniature2 = game?.background_image;
-  const miniature3 = game?.background_image_additional;
 
   return (
     <article className="gamepage">
@@ -56,28 +47,7 @@ export default function GamePage() {
         <img
           className="main-image"
           src={mainImage}
-          alt={`${game?.name} num 1`}
-        ></img>
-        <section className="miniatures">
-          <img
-            className="miniature"
-            style={{ maxWidth: "250px" }}
-            src={miniature1}
-            alt={`${game?.name} num 2`}
-          ></img>
-          <img
-            className="miniature"
-            style={{ maxWidth: "250px" }}
-            src={miniature2}
-            alt={`${game?.name} num 3`}
-          ></img>
-          <img
-            className="miniature"
-            style={{ maxWidth: "250px" }}
-            src={miniature3}
-            alt={`${game?.name} num 4`}
-          ></img>
-        </section>
+          alt={game?.name}></img>
       </figure>
 
       <section className="textarea">
@@ -98,12 +68,13 @@ export default function GamePage() {
         <p className="plot">{game?.description_raw}</p>
         <GameTable game={game} />
         <section className="tag-group">
-          Tags:
-          <span className="tags">platformer</span>
-          <span className="tags">jump</span>
-          <span className="tags">stressful</span>
+          {game?.tags.slice(0,5).map((tag, index)=>(
+            <span className="tags" key={index}>{tag.name}</span>
+          )  
+        
+          )}
         </section>
-        <button className="btn btn-outline-primary">Buy</button>
+        {/*<button className="btn btn-outline-primary">Buy</button>*/}
       </section>
     </article>
   );
