1693fbe8bc4ec14e5c3d96568424c133faebb2f6
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-05-05 10:48:36 +0200

    GameCard & DashBoard updates + some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,34 +2,16 @@
 import "./App.css";
 import "./css/main.css";
 import HomePage from "./pages/HomePage";
-import GamePage from './components/GamePage'
+import GamePage from "./components/GamePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  // const [games, setGames] = useState([]);
-
-  // //Fetching 3 newest games from 01.01.2023 until 27.04.2023
-  // //Will change to dynamic fetch later if needed
-  // const getGames = async () => {
-  //   const response = await fetch(
-  //     `https://api.rawg.io/api/games?key=d2d5f79e22a6464d852e6cd6b671c8d7&ordering=-released&page_size=3&dates=2023-01-01,2023-04-27`
-  //   );
-  //   const data = await response.json();
-  //   setGames(data.results);
-  // };
-
-  // useEffect(() => {
-  //   getGames();
-  // }, []);
-
-  // console.log(games);
-
   //Cecilie: har satt inn GamePage her for å sjekke link/path. Bare å flytte/ordne
   return (
     <Router>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path=":slug" element={<GamePage/>}/>
+        <Route path=":slug" element={<GamePage />} />
       </Routes>
     </Router>
   );
