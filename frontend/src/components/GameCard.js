dd2c671126634aa0f590fb78ba28a98429a74a44
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-05-05 10:48:45 +0200

    DashBoard + GameCard

--- frontend/src/components/GameCard.js
+++ frontend/src/components/GameCard.js
@@ -1,35 +1,43 @@
 import React from "react";
 import { GiDuration } from "react-icons/gi";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 //Komponent for forhåndsvisning av spill som kan brukes på forsiden av dashboard og på spillsidene
 //Skal ha en lenke basert på apiId videre til GamePage
 //Henter inn et spillobjekt som props
 //Sjanger må senere basereres på filtrering? På objektet er det en liste med sjangere
 //Se på hvordan vi velger bilde. Bruker foreløpig kun første der det er en liste med flere
-export default function GameCard({game}) {
-  const image = game?.short_screenshots[0]?.image
-  const title = game?.name
-  const genre = game?.genres[0]?.name
-  const playTime = game?.playtime
-  const id = game?.id
-  
-  console.log(id)
+export default function GameCard({
+  title,
+  genre,
+  image,
+  game,
+  playTime,
+  text,
+}) {
   return (
+    <article className="gamecard">
+      <img className="gamecard-image" src={image} alt="game"></img>
 
-      <article className="gamecard">
-        <img className="gamecard-image" src={image} alt="game" style={{maxWidth: '200px'}}></img>
-  
-        <section className="gamecard-text">
-          <h3>{title}</h3>
-          <h4>{genre}</h4>
-          <section className="playtime">
-            <GiDuration className="duration-icon" size={28} alt="playtime"/>
-            <p>{playTime}</p>
-          </section>
-           <Link to={game?.slug}>Tittel</Link>
+      <section className="gamecard-text">
+        <h3>{title}</h3>
+        <h5>Genres:</h5>
+        <ul>{genre}</ul>
+        <section className="playtime">
+          {playTime === 0 ? null : (
+            <>
+              <GiDuration className="duration-icon" size={28} alt="playtime" />
+              <p> {playTime} hours</p>
+            </>
+          )}
         </section>
-      </article>
- 
-  )
+        <section className="button-section">
+          <Link to={game?.slug} class="btn btn-outline-primary">
+            Read more
+          </Link>
+          <button class="btn btn-outline-primary">{text}</button>
+        </section>
+      </section>
+    </article>
+  );
 }
