b6706c4c2e87f513b015856f2ed1aa2461b7b697
Cecilie Seim <totilie8@gmail.com>
2023-05-12 20:30:20 +0200

    Services

--- frontend/src/utils/sanity/userServices.js
+++ frontend/src/utils/sanity/userServices.js
@@ -30,7 +30,17 @@ export const getGamesByUsername = async (username) => {
 //Midlertidig forsøk på å hente ut filmer fra bruker med sjangere
 export const fetchGamesWithGenresByUsername = async (username) => {
     try{
-        const result = await client.fetch(`*[_type == 'user' && username == $username]{games[]->{"title": title, "genres": genres[]->{title}}}`, { username});
+        const result = await client.fetch(`*[_type == 'user' && username == $username]{games[]->{..., "genres": genres[]->{title}}}`, { username});
+        return result;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export const fetchGamesFilter = async (username) => {
+    try{
+        const result = await client.fetch(`*[_type == 'user' && username == $username][0] {games[]->{"sjanger": *[_type == "genre" && references(^._id)].title}}`, { username});
         return result;
     }
     catch (error) {
