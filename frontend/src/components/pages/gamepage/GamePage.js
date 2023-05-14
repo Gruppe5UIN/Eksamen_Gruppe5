80729593da317add53c1c62d7d52411c5a97c763
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-05-14 23:37:22 +0200

    Loading on GamePage

--- frontend/src/components/pages/gamepage/GamePage.js
+++ frontend/src/components/pages/gamepage/GamePage.js
@@ -16,86 +16,102 @@ import WordCloud from "./WordCloud";
 //Button funksjonalitet må lages
 //Trenger error håndtering
 
-export default function GamePage({favourites, setFavourites}) {
+export default function GamePage({ favourites, setFavourites }) {
   const { slug } = useParams();
 
   const [game, setGame] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
- const url = `https://api.rawg.io/api/games/${slug}?key=6ccebb406ca942cd8ddc8584b1da9a4f`;
- 
+  const url = `https://api.rawg.io/api/games/${slug}?key=6ccebb406ca942cd8ddc8584b1da9a4f`;
+
   const getGame = async () => {
     const response = await fetch(url);
     const data = await response.json();
     setGame(data);
   };
- 
- 
+
   //Håndterer et klikk på favoritt ikon - legger til hvis den ikke er favoritt, fjerner hvis den allerede er favoritt
   //Ligger en midlertidig print til console når man legger til/fjerner favoritt
   //Lagrer et game object
-  const handleFavourite = (event) => {  
-      event.preventDefault()
-      if(favourites.some(item => item['id'] === game?.id)){
-        setFavourites(
-          favourites.filter(item => item.id !== game?.id));
-        console.log(`${game?.name} er fjernet fra favoritter`)
-      }
-     else{
-        setFavourites( 
-          [ 
-            ...favourites, 
-            { id: game?.id, name: game?.name,image: game?.background_image, slug: game?.slug} 
-          ]
-        );
-        console.log(`${game?.name} er min favoritt`);
-        }
-  }
+  const handleFavourite = (event) => {
+    event.preventDefault();
+    if (favourites.some((item) => item["id"] === game?.id)) {
+      setFavourites(favourites.filter((item) => item.id !== game?.id));
+      console.log(`${game?.name} er fjernet fra favoritter`);
+    } else {
+      setFavourites([
+        ...favourites,
+        {
+          id: game?.id,
+          name: game?.name,
+          image: game?.background_image,
+          slug: game?.slug,
+        },
+      ]);
+      console.log(`${game?.name} er min favoritt`);
+    }
+  };
 
   useEffect(() => {
-    
-    getGame()
-      // eslint-disable-next-line
-  },[])
+    setIsLoading(true);
+    getGame();
+    setIsLoading(false);
+    // eslint-disable-next-line
+  }, []);
 
   return (
-    <article className="gamepage">
-      <figure className="image-frame">
-        <img
-          className="main-image"
-          src={game?.background_image}
-          alt={game?.name}></img>
-      </figure>
+    <>
+      {isLoading ? (
+        <div className="loader-wrapper">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <>
+          <article className="gamepage">
+            <figure className="image-frame">
+              <img
+                className="main-image"
+                src={game?.background_image}
+                alt={game?.name}
+              ></img>
+            </figure>
 
-      <section className="textarea">
-        <header className="gamepage-header">
-          <h1>{game?.title}</h1>
-          <section className="game-details">
-            {game?.rating ? 
-            <span className="rating">
-              <p>{game?.rating}</p>
-            </span> : ''}
-            <GiHeartShield
-              onClick={handleFavourite}
-              className="favourite-icon"
-              size={42}
-              alt="favourite"
-            />
-          </section>
-        </header>
+            <section className="textarea">
+              <header className="gamepage-header">
+                <h1>{game?.title}</h1>
+                <section className="game-details">
+                  {game?.rating ? (
+                    <span className="rating">
+                      <p>{game?.rating}</p>
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  <GiHeartShield
+                    onClick={handleFavourite}
+                    className="favourite-icon"
+                    size={42}
+                    alt="favourite"
+                  />
+                </section>
+              </header>
 
-        <p className="plot">{game?.description_raw}</p>
+              <p className="plot">{game?.description_raw}</p>
 
-        <GameTable game={game} />
-        {game?.tags !== undefined ? (
-        
-            <section className="tag-group">
-                <WordCloud gameTags={game?.tags}/>
+              <GameTable game={game} />
+              {game?.tags !== undefined ? (
+                <section className="tag-group">
+                  <WordCloud gameTags={game?.tags} />
+                </section>
+              ) : (
+                ""
+              )}
+
+              <button className="btn btn-outline-primary">Buy</button>
             </section>
-
-        ) : ''}
-      
-        <button className="btn btn-outline-primary">Buy</button>
-      </section>
-    </article>
+          </article>
+        </>
+      )}
+    </>
   );
 }
