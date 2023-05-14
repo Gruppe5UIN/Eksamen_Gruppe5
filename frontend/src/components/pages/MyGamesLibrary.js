76c558ad1176eabab2558b1a296803c3fc31707f
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2023-05-14 19:05:37 +0200

    Filter now works

--- frontend/src/components/pages/MyGamesLibrary.js
+++ frontend/src/components/pages/MyGamesLibrary.js
@@ -6,6 +6,7 @@ import UserContext from "../../context/UserContext";
 export default function MyGamesLibrary() {
   const { user } = useContext(UserContext);
   const [games, setGames] = useState([]);
+  const [filteredGames, setFilteredGames] = useState([]);
   const [selected, setSelected] = useState("");
 
   const getGames = async (username) => {
@@ -19,47 +20,69 @@ export default function MyGamesLibrary() {
       getGames(user.username)
         .then((games) => {
           setGames(games);
+          setFilteredGames(games);
         })
         .catch((error) => {
           console.error(error);
           window.location.href = "/login";
         });
     }
+    console.log(games);
+    // eslint-disable-next-line
   }, [user]);
+
+  useEffect(() => {
+    const filtered = getFilteredGames();
+    setFilteredGames(filtered);
+    // eslint-disable-next-line
+  }, [selected]);
 
   const handleSelect = (e) => {
     e.preventDefault();
     setSelected(e.target.value);
   };
 
-  useEffect(() => {
-    console.log(selected);
-  }, [selected]);
+  const getFilteredGames = () => {
+    if (selected === "all") {
+      return games;
+    } else {
+      return games.filter((game) =>
+        game.game.genres.some((genre) => genre.title === selected)
+      );
+    }
+  };
+
+  const uniqueGenres = new Set();
 
   return (
     <section className="page-container">
       <h3 id="gs-first">My Games Library - {games.length} games</h3>
-
       <select id="genre-filter" onChange={handleSelect}>
-        <option>All genres</option>
+        <option value="all">All genres</option>
         {games?.map((item, index) => {
           return (
             <React.Fragment key={index}>
-              {item?.game.genres.map((genre, index) => (
-                <option value={genre.id} key={index}>
-                  {genre.title}
-                </option>
-              ))}
+              {item?.game.genres.map((genre) => {
+                if (!uniqueGenres.has(genre.title)) {
+                  uniqueGenres.add(genre.title);
+                  return (
+                    <option value={genre.title} key={genre.title}>
+                      {genre.title}
+                    </option>
+                  );
+                }
+                return null;
+              })}
             </React.Fragment>
           );
         })}
       </select>
-      {games?.map((item) => (
+      {filteredGames?.map((item) => (
         <GameCard
           key={item._id}
           title={item.game.title}
-          genre={item.game.genres.map((genre, index) => (
-            <li key={index}>{genre.title}</li>
+          genre={item.game.genres.map((genre) => (
+            <li key={genre.title}>{genre.title}</li>
           ))}
           image={item.game.image}
           slug={item.game.slug.current}
