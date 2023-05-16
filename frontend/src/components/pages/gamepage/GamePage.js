2f03e1d14b55facc137cb30830b90c9feb4fd220
Julian Alsemmani <juliankonge@hotmail.com>
2023-05-16 15:47:06 +0200

    Update GamePage.js

--- frontend/src/components/pages/gamepage/GamePage.js
+++ frontend/src/components/pages/gamepage/GamePage.js
@@ -16,6 +16,7 @@ export default function GamePage({ favourites, setFavourites }) {
   const [isLoading, setIsLoading] = useState(true);
   const [Icon, setIcon] = useState(false);
   const [buttonText, setButtonText] = useState("Buy this game");
+  const [favouriteVisible, setFavouriteVisible] = useState(false);
   const { user } = useContext(UserContext);
 
   const url = `https://api.rawg.io/api/games/${slug}?key=6ccebb406ca942cd8ddc8584b1da9a4f`;
@@ -78,6 +79,7 @@ export default function GamePage({ favourites, setFavourites }) {
         if (result) {
           // Hvis resultatet er true, setter vi buttonText til "Spillet er i din liste"
           setButtonText("Spillet er i din liste");
+          setFavouriteVisible(true);
         }
       })
 
@@ -148,8 +150,9 @@ export default function GamePage({ favourites, setFavourites }) {
                   ) : (
                     ""
                   )}
-                  {Icon ? (
-                    <>
+
+                  {favouriteVisible ?
+                    (Icon ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="30"
@@ -164,10 +167,8 @@ export default function GamePage({ favourites, setFavourites }) {
                           d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                         />
                       </svg>
-                    </>
-                  ) : (
-                    <>
-                      {" "}
+
+                    ) : (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="30"
@@ -179,8 +180,11 @@ export default function GamePage({ favourites, setFavourites }) {
                       >
                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                       </svg>
-                    </>
+                    )
+                  ) : (
+                  <></>
                   )}
+
                 </section>
               </header>
 
