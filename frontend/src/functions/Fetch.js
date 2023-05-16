6571bf6754f7094da0cfb5b9aff5b5f4c1a68401
Cecilie Seim <totilie8@gmail.com>
2023-05-16 15:04:29 +0200

    try/catch Fetch

--- frontend/src/functions/Fetch.js
+++ frontend/src/functions/Fetch.js
@@ -2,10 +2,14 @@
 //Dates må med som fast parameter, fordi RAWG har publisert mange testspill med datoer i fremtiden som vi aldri vil ha med
 
 export const getGames = async (params) => {
-  const queryParams = new URLSearchParams(params).toString();
-  const response = await fetch(
-    `https://api.rawg.io/api/games?key=6ccebb406ca942cd8ddc8584b1da9a4f&${queryParams}&dates=2023-01-01,2023-05-01`
-  );
-  const data = await response.json();
-  return data.results;
+  try{
+      const queryParams = new URLSearchParams(params).toString();
+      const response = await fetch(
+        `https://api.rawg.io/api/games?key=6ccebb406ca942cd8ddc8584b1da9a4f&${queryParams}&dates=2023-01-01,2023-05-01`
+      );
+      const data = await response.json();
+      return data.results;
+    } catch (error) {
+      console.log(error)
+    }
 };
